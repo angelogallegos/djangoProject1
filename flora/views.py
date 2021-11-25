7f8adf6df047475257a6f0abc63d4e658c3fd27d
eduardo.gonzalez5 <eduardo.gonzalez5@virginiogomez.cl>
2021-11-25 19:13:45 -0300

    Base primordial

--- flora/views.py
+++ flora/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def mostrar_especies(request):
-    return render(request, 'mostrar_flora.html')
+    return render(request, 'mostrar_especie.html')
 
 
 
