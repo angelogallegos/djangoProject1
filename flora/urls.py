c2f15b7c01b9d6e916093cc81e7fe30f6de2d65a
eduardo.gonzalez5 <eduardo.gonzalez5@virginiogomez.cl>
2021-11-26 05:06:59 -0300

    Eduardo - Formularios parte 1

--- flora/urls.py
+++ flora/urls.py
@@ -4,11 +4,11 @@ from flora import  views
 urlpatterns = [
     path('', views.mostrar_especie, name='mostrar_especie'),
     path('agregar/', views.agregar_especie, name='agregar_especie'),
-    path('', views.mostrar_tipo_especie, name='mostrar_tipo_especie'),
-    path('agregar/', views.agregar_tipo_especie, name='agregar_tipo_especie'),
-    path('', views.mostrar_origen, name='mostrar_origen'),
-    path('agregar/', views.agregar_origen, name='agregar_origen'),
-    path('', views.mostrar_zona, name='mostrar_zona'),
-    path('agregar/', views.agregar_zona, name='agregar_zona')
+    path('lista/tipo', views.mostrar_tipo_especie, name='mostrar_tipo_especie'),
+    path('agregar/tipo/', views.agregar_tipo_especie, name='agregar_tipo_especie'),
+    path('lista/origen', views.mostrar_origen, name='mostrar_origen'),
+    path('agregar/origen/', views.agregar_origen, name='agregar_origen'),
+    path('lista/zona', views.mostrar_zona, name='mostrar_zona'),
+    path('agregar/zona/', views.agregar_zona, name='agregar_zona')
 
 ]
